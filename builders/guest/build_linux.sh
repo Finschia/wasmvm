b86c95057fec061464eb35610e58a665453ed87f
Jiyong Ha <jiyong.ha@linecorp.com>
2021-04-07 18:29:16 +0900

    build: support cross-compile for private repo
    Remove docker -u option when cargo build
    because it occurs git config failure.

--- builders/guest/build_linux.sh
+++ builders/guest/build_linux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git config --global url."https://${GITHUB_TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
 cargo build --release
 cp target/release/deps/libwasmvm.so api
 # FIXME: re-enable stripped so when we approach a production release, symbols are nice for debugging
