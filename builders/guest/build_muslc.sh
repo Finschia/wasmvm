d651e78685c9f67063985fc09e4e2908356d7ed9
Jiyong Ha <70933233+brew0722@users.noreply.github.com>
2021-04-08 15:28:26 +0900

    build: support cross build for priv repo (#14)
    * build: support cross-compile for private repo
    
    Remove docker -u option when cargo build
    because it occurs git config failure.
    
    * build: update linux prebuilt binary
    
    used 'make release-build-linux'
    
    * build: update macos prebuilt binary
    
    used 'make release-build-macos'

--- builders/guest/build_muslc.sh
+++ builders/guest/build_muslc.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+git config --global url."https://${GITHUB_TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
 cargo build --release --example muslc
 cp /code/target/release/examples/libmuslc.a /code/api/libwasmvm_muslc.a
