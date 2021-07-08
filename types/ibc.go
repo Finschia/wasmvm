1be6cd32f2123a3296efae8f8680e4cfa39dcced
shiki.takahashi <shiki.takahashi@linecorp.com>
2021-07-08 11:10:52 +0900

    fix: build error

--- types/ibc.go
+++ types/ibc.go
@@ -110,11 +110,7 @@ type IBCReceiveResult struct {
 // Where the Acknowledgement bytes contain an encoded error message to be returned to
 // the calling chain. (Returning IBCReceiveResult::Err will abort processing of this packet
 // and not inform the calling chain).
-<<<<<<< HEAD
-// This is the counterpart of (IbcReceiveResponse)(https://github.com/line/cosmwasm/blob/v0.14.0-0.4.0/packages/std/src/ibc.rs#L231-L254).
-=======
 // This is the counterpart of `IbcReceiveResponse` in https://github.com/line/cosmwasm/blob/main/packages/std/src/ibc.rs .
->>>>>>> 6e065e603814ed6813b56e1e4f17593a43d0e09d
 type IBCReceiveResponse struct {
 	// binary encoded data to be returned to calling chain as the acknowledgement
 	Acknowledgement []byte `json:"acknowledgement"`
