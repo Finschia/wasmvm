76f849e6673d1706605b8aebfd37963d04b4505b
Yongwoo Lee <whylee@linecorp.com>
2021-07-15 16:39:23 +0900

    chore: remove private repo dependencies (#43)

--- builders/guest/build_linux.sh
+++ builders/guest/build_linux.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git config --global url."https://${GITHUB_TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
 cargo build --release
 cp target/release/deps/libwasmvm.so api
 # FIXME: re-enable stripped so when we approach a production release, symbols are nice for debugging
