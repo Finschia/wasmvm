85453303cf7d1ca11c1f3da9da03e611ac08a209
shiki <shiki.takahashi@linecorp.com>
2022-06-28 17:52:57 +0900

    build: version bump to 1.0.0-0.10.0 (#66)
    * build: version bump to 1.0.0-0.10.0
    
    * fix: fix builders and add build tag for golang v1.15.0

--- api/link_mac.go
+++ api/link_mac.go
@@ -1,5 +1,5 @@
-//go:build !static || darwin
-// +build !static darwin
+//go:build darwin
+// +build darwin
 
 package api
 
--- builders/guest/build_linux_static.sh
+++ builders/guest/build_linux_static.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+set -e # Note we are not using bash here but the Alpine default shell
 
-cargo build --release --example staticlib
+echo "Starting aarch64-unknown-linux-musl build"
+export CC=/opt/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc
+cargo build --release --target aarch64-unknown-linux-musl --example staticlib
+unset CC
+
+echo "Starting x86_64-unknown-linux-musl build"
+cargo build --release --target x86_64-unknown-linux-musl --example staticlib
+
+cp target/aarch64-unknown-linux-musl/release/examples/libstaticlib.a artifacts/libwasmvm_static.aarch64.a
+cp target/x86_64-unknown-linux-musl/release/examples/libstaticlib.a artifacts/libwasmvm_static.a
