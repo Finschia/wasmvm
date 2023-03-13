e04a9ffd9a50e8b940b8eb52bd103ef68ffd64eb
zemyblue <zemyblue@gmail.com>
2023-03-13 21:40:29 +0900

    ci: add ci test about compiled internal libraries.
    Signed-off-by: zemyblue <zemyblue@gmail.com>

--- lib_test.go
+++ lib_test.go
@@ -281,5 +281,5 @@ func TestGetMetrics(t *testing.T) {
 func TestLibwasmvmVersion(t *testing.T) {
 	version, err := LibwasmvmVersion()
 	require.NoError(t, err)
-	require.Equal(t, version, "1.1.1-0.11.2")
+	require.Equal(t, "1.1.1-0.11.2", version)
 }
